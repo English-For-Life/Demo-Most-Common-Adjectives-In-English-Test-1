8f23d20a48941cb035f156d443cf698014828c62
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-11 16:52:04 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,18 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Mike is a ............... student. He always gets good grades.",
+        options:["hard-working", "elementary", "standart", "lazy", "good-looking"],
+        answer: 0
+    },
+    {
+        q:"He crashed his car into a tree. It was badly ................    .",
+        options:["hopeless", "damaged", "hurt", "wounded", "injured"],
+        answer: 1
+    },
+    {
+        q:"It was a ............... question. I couldn't understand a word of it.",
+        options:["necessary", "unnecessary", "difficult", "easy", "essential"],
+        answer: 2
+    }
+]
